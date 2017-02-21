6efe9b3182f5a9db854ca275047729868f07a2e7
Steven Ma <7348348564@stevenma.xyz>
2017-02-21 18:36:33 -0500

    Make TypeTester singleton

--- type_tester.hpp
+++ type_tester.hpp
@@ -1,3 +1,40 @@
-class TypeTester {
+#pragma once
 
-};
+/*
+ * Global keeps all the global variables
+ *
+ * Singleton design, only one instance can be created
+ *
+ * When scope ends (program finish/Ctrl-c) the destructor will run
+ */
+
+typedef class TypeTester {
+public:
+
+    void run_type_tester();
+
+private:
+
+    TypeTester();
+
+    /*
+     * Reads common word list
+     */
+    void get_words();
+
+    /*
+     * Promptly print console to terminal
+     */
+    void print_console();
+
+public:
+
+    static TypeTester& get_inst() {
+        static TypeTester inst;
+        return inst;
+    }
+
+    TypeTester(TypeTester const&) = delete;
+    void operator=(TypeTester const&) = delete;
+
+} type_tester_t;
